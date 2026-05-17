dbba2bddde15b523170d9a807070aa198a6a4327
code043 <procoder043@gmail.com>
2026-05-17 11:09:39 -0300

    refactor: navbar screen size

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,19 @@
 import { Menu } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <header className="w-full flex items-center justify-between px-6 py-4 shadow-sm bg-white">
       <h2 className="text-center text-4xl font-serif tracking-tight leading-tight">NORDIC ROSE</h2>
-      <Menu />
+      <nav className="hidden md:block">
+        <ul className="font-serif space-x-8">
+          <Link href={'/'}>BLOG</Link>
+          <Link href={'/about'}>ABOUT</Link>
+          <Link href={'/links'}>LINKS</Link>
+          <Link href={'/projects'}>PROJECTS</Link>
+        </ul>
+      </nav>
+      <Menu className="md:hidden"/>
     </header>
   );
 }

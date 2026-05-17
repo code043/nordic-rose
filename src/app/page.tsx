a9ddf25639db3834082bdcd89ad92b173808d89b
code043 <procoder043@gmail.com>
2026-05-16 22:37:46 -0300

    feat: hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Nordic Rose</div>
+    <section className="py-10">
+      <div className="flex justify-center mt-10">
+        <Image
+          src="/hero.png"
+          alt="imagem"
+          width={800}
+          height={500}
+          className=" "
+        />
+      </div>
+    </section>
   );
 }
